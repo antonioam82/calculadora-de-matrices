1aab6f24a25ed4e327265267d3e55f2122d04111
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-07-05 19:55:00 +0200

    Update calculadora_matrices.py

--- calculadora_matrices.py
+++ calculadora_matrices.py
@@ -3,7 +3,8 @@ from VALID import OK, OKI, ns
 import numpy as np
 import subprocess
 
-def crea_matriz(fil,col,f,c):
+def crea_matriz(fil,col):
+    f=-1;c=-1
     e_fil=[]
     for f in range(0,fil):
         e_col=[]
@@ -13,35 +14,47 @@ def crea_matriz(fil,col,f,c):
             valor=OK(input('Introduzca el componente (%d,%d): '%(f,c)))
             e_col.append(valor)
         e_fil.append(e_col)
-        #for i in e_fil:
-            #print(i)
     return e_fil
     
 while True:
+    print("         CALCULADORA DE MATRICES          ") 
+    print("""************TABLA DE OPERADORES************
+*******************************************
+SUMA                           OPERADOR "+"
+RESTA                          OPERADOR "-"
+DIVISION                       OPERADOR "/"
+EXPONENTE                    OPERADOR "EXP"
+*******************************************
+*******************************************""")
+    
     fil=OKI(input("Indique número de filas: "))
     col=OKI(input("Indique número de columnas: "))
 
     f=-1;c=-1
     acum=0
-    matr=crea_matriz(fil,col,f,c)
+    matr=crea_matriz(fil,col)
     acum=np.array([matr],float)
     print(acum)
     while True:
         oper=input("Introduzca operador: ")
-        while oper!="+" and oper!="-" and oper!="/" and oper!="=":
+        while oper!="+" and oper!="-" and oper!="/" and oper!="EXP" and oper!="=":
             oper=input("Introduzca un operador válido: ")
         if oper=="+":
-            matr=crea_matriz(fil,col,f,c)
+            matr=crea_matriz(fil,col)
             matri=np.array([matr],float)
             acum+=matri
         elif oper=="-":
-            matr=crea_matriz(fil,col,f,c)
+            matr=crea_matriz(fil,col)
             matri=np.array([matr],float)
             acum-=matri
         elif oper=="/":
-            matr=crea_matriz(fil,col,f,c)
+            matr=crea_matriz(fil,col)
             matri=np.array([matr],float)
             acum/=matri
+        elif oper=="EXP":
+            matr=crea_matriz(fil,col)
+            matri=np.array([matr],float)
+            acum**=matri
         elif oper=="=":
             print("MATRIZ RESULTADO")
             print(acum)
@@ -53,6 +66,7 @@ while True:
     matr=0
     subprocess.call(["cmd.exe","/C","cls"])
         
+
     
 
 
