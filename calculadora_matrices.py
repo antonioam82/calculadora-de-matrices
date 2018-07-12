3404cc436d11e86e03ec5f7fccf0094f228ce411
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2018-07-12 15:32:24 +0200

    Update calculadora_matrices.py

--- calculadora_matrices.py
+++ calculadora_matrices.py
@@ -14,6 +14,7 @@ def crea_matriz(fil,col):
             valor=OK(input('Introduzca el componente (%d,%d): '%(f,c)))
             e_col.append(valor)
         e_fil.append(e_col)
+    #print(e_fil)
     return e_fil
     
 while True:
@@ -22,13 +23,15 @@ while True:
 *******************************************
 SUMA                           OPERADOR "+"
 RESTA                          OPERADOR "-"
+MULTIPLICACION                 OPERADOR "*"
 VER RESULTADO                  OPERADOR "="
 *******************************************
 *******************************************""")
     
     fil=OKI(input("Indique número de filas: "))
     col=OKI(input("Indique número de columnas: "))
-
+    e=fil
+    
     f=-1;c=-1
     acum=0
     matr=crea_matriz(fil,col)
@@ -36,24 +39,32 @@ VER RESULTADO                  OPERADOR "="
     print(acum)
     while True:
         oper=input("Introduzca operador: ")
-        while oper!="+" and oper!="-" and oper!="=":
+        while oper!="+" and oper!="-" and oper!="*" and oper!="=":
             oper=input("Introduzca un operador válido: ")
         if oper=="+":
             matr=crea_matriz(fil,col)
             matri=np.array([matr],float)
-            acum+=matri
+            acum=acum+matri
         elif oper=="-":
             matr=crea_matriz(fil,col)
             matri=np.array([matr],float)
-            acum-=matri
+            acum=acum-matri
+        elif oper=="*":
+            fil=col
+            matr=crea_matriz(fil,col)
+            matri=np.array([matr],float)
+            acum=np.dot(acum,matri)
+            fil=e
         elif oper=="=":
+            print("")
             print("MATRIZ RESULTADO")
             print(acum)
+            print("")
             break
         print(matri)
     conti=ns(input("¿Reiniciar programa?: "))
     if conti=="n":
         break
     matr=0
-    subprocess.call(["cmd.exe","/C","cls"])
-        
+    subprocess.call(["cmd.exe","/C","cls"])       
+    
