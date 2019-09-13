cf88eb3ae0a481ab023c27434d4e4136ee1f3886
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-09-13 19:35:37 +0200

    Create calcu_matricesB.py

---
+++ calcu_matricesB.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np
+import subprocess
+
+def OKI(n):
+    try:
+        n=int(n)
+    except:
+        n=OKI(input("Caracter no valido: "))
+    return n
+
+def OK(n):
+    try:
+        n=float(n)
+    except:
+        n=OK(input("Caracter no valido: "))
+    return n
+
+def ns(c):
+    while c!=("s") and c!=("n"):
+        print(chr(7));c=input("Escribe solo \'n\' o \'s\' según su opción: ")
+    return(c)
+
+def val(tp):
+    while tp!="N" and tp!="M":
+        tp=input("Introduzca \'N\' para dato numérico y \'M\' para matriz: ")
+    return tp
+
+#FUNCIÓN DE TIPO DE DATO
+def dato():
+    tipo_dato=val(input("Tipo de dato: "))
+    if tipo_dato=="M":
+        matr=crea_matriz(fil,col)
+    else:
+        matr=OK(input("Introduce número: "))
+    return matr
+
+#FUNCIÓN PARA DEFINIR MATRIZ
+def crea_matriz(fil,col):
+    while True:
+        valores = list(map(float, input("Introduce valores separados por espacio: ").split()))
+        if len(valores)!= fil*col:
+            print("EL NÚMERO DE VALORES INTRODUCIDO NO SE CORRESPONDE CON LAS DIMENSIONES ESPECIFICADAS")
+        else:
+            matriz = np.array(valores).reshape(fil,col)
+            break
+    return matriz
+    
+while True:
+    print("          CALCULADORA DE MATRICES          ") 
+    print("""************TABLA DE OPERADORES************
+*******************************************
+SUMA                           OPERADOR "+"
+RESTA                          OPERADOR "-"
+MULTIPLICACION                 OPERADOR "*"
+VER RESULTADO                  OPERADOR "="
+DATO MATRIZ                    OPERANDO "M"
+DATO NÚMERO                    OPERANDO "N"
+*******************************************
+*******************************************""")
+    
+    fil=OKI(input("Indique número de filas: "))
+    col=OKI(input("Indique número de columnas: "))
+    e=fil
+    f=-1;c=-1
+    acum=crea_matriz(fil,col)
+    print("\nOPERANDO")
+    print(acum,"\n")
+    while True:
+        oper=input("Introduzca operador: ")
+        while oper!="+" and oper!="-" and oper!="*" and oper!="=":
+            oper=input("Introduzca un operador válido: ")
+        if oper=="+":
+            matr=dato()
+            acum=acum+matr
+        elif oper=="-":
+            matr=dato()
+            acum=acum-matr
+        elif oper=="*":
+            tipo_dato=val(input("Tipo de dato: "))
+            if tipo_dato=="M":
+                fil=col
+                col=OKI(input("Introduce número de columnas: "))
+                matr=crea_matriz(fil,col)
+                acum=np.dot(acum,matr)
+                fil=e
+            else:
+                matr=OK(input("Introduce número: "))
+                acum=acum*matr
+        elif oper=="=":
+            print("\nMATRIZ RESULTADO")
+            print(acum,"\n")
+            break
+        print("\nOPERANDO")
+        print(matr,"\n")
+        
+    conti=ns(input("¿Reiniciar cálculos?: "))
+    if conti=="n":
+        break
+    matr=0
+    subprocess.call(["cmd.exe","/C","cls"])
